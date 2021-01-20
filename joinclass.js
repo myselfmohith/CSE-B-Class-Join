11452799c4b7422897553d86080a27ec238ad79c
myselfmohith <bezawadamohith14@gmail.com>
2021-01-20 15:34:58 +0530

    🔔 Added notification option
    >> Need to check for a whie and use

--- joinclass.js
+++ joinclass.js
@@ -1,5 +1,6 @@
 const span = document.querySelector(".pname");
 
+// Jason File here >>>>>
 const info = `{
     "links": {
       "INT18R371": "https://meet.google.com/obf-rgqt-tpt",
@@ -7,7 +8,8 @@ const info = `{
       "CSE18R273": "https://meet.google.com/wqr-ecgm-awx",
       "CSE18R260": "https://meet.google.com/pyf-ftxx-cdc",
       "MAT18R207": "https://meet.google.com/pbo-ubqb-zjm",
-      "BIT18R101": "https://meet.google.com/nuq-jzvn-efk"
+      "BIT18R101": "https://meet.google.com/nuq-jzvn-efk",
+      "NONE": "index.html"
     },
     "1": {
       "Day": "Monday",
@@ -35,11 +37,14 @@ const info = `{
       "2": "BIT18R101",
       "3": "CSE18R173",
       "4": "CSE18R260",
-      "6": "MAT18R207"
+      "5": "NONE",
+      "6": "MAT18R207",
+      "7": "NONE"
     },
     "4": {
       "Day": "Thrusday",
       "1": "BIT18R101",
+      "2": "NONE",
       "3": "CSE18R273",
       "4": "CSE18R273",
       "5": "CSE18R173",
@@ -63,32 +68,75 @@ const info = `{
   }
   `;
 
+// -------- Notification Sending Part  -----------------
+if (Notification.permission === "default") {
+  setTimeout(() => {
+    Notification.requestPermission();
+  }, 4000);
+}
+
+function notifyStudent(cclass) {
+  const noti = new Notification("CSE-B KLU CLASS ALERT", {
+    body: "You have " + cclass + " class in Few Minutes",
+    icon:
+      "https://upload.wikimedia.org/wikipedia/en/5/53/Kalasalingam_Academy_of_Research_and_Education_logo.png",
+  });
+}
+// ---------------------------------------------------
+
 function parseTime(text) {
-    var mxm = new Date();
-    var c = text.split(":");
-    mxm.setHours(Number(c[0]), Number(c[1]), 0)
-    return mxm;
+  var mxm = new Date();
+  var c = text.split(":");
+  mxm.setHours(Number(c[0]), Number(c[1]), 0,0);
+  return mxm;
+}
+
+function parseText(date) {
+  var res = date.getHours() + ":" + date.getMinutes();
+  return res;
 }
 
 const timetable = JSON.parse(info);
-tstart = [parseTime("8:57"),parseTime("9:57"),parseTime("10:57"),parseTime("11:57"),parseTime("13:57"),parseTime("14:48"),parseTime("15:38")]
-tend = [parseTime("9:50"),parseTime("10:50"),parseTime("11:50"),parseTime("12:50"),parseTime("14:48"),parseTime("15:38"),parseTime("16:30")]
-var cclass , clink ,forhtml;
+const tstart = [
+  parseTime("8:57"),
+  parseTime("9:57"),
+  parseTime("10:57"),
+  parseTime("11:57"),
+  parseTime("13:57"),
+  parseTime("14:48"),
+  parseTime("15:38"),
+];
+const tend = [
+  parseTime("9:50"),
+  parseTime("10:50"),
+  parseTime("11:50"),
+  parseTime("12:50"),
+  parseTime("14:48"),
+  parseTime("15:38"),
+  parseTime("16:30"),
+];
+var cclass, clink, forhtml;
 
 function changeClass() {
-    var xyz = new Date();
-    for (var i = 0; i < 7; i++){
-        if (xyz>=tstart[i] && xyz<tend[i]) {
-            cclass = timetable[xyz.getDay()][i+1]; 
-            clink = timetable["links"][cclass];
-            forhtml = "<a target='_blank' onClick='javascript:setTimeout(window.close, 1);' href='" + clink + "'>" + cclass + "</a";
-            span.innerHTML = forhtml;
-            return;
-      }
+  var xyz = new Date();
+  for (var i = 0; i < 7; i++) {
+    if (xyz >= tstart[i] && xyz < tend[i]) {
+      cclass = timetable[xyz.getDay()][i + 1];
+      if (cclass === "NONE") break;
+      clink = timetable["links"][cclass];
+      forhtml ="<a target='_blank' onClick='javascript:setTimeout(window.close, 1);' href='" +clink +"'>" +cclass +"</a";
+      span.innerHTML = forhtml;
+
+      // For Notification -----------
+      var l1 = parseText(xyz)
+      var l2 = parseText(tstart[i])
+      if (l1 === l2) notifyStudent(cclass);
+      return;
     }
-    forhtml = "NONE";
-    span.innerHTML = forhtml;
+  }
+  forhtml = "NONE";
+  span.innerHTML = forhtml;
 }
 
 setTimeout(changeClass, 0);
-setInterval(changeClass, 30000);
+setInterval(changeClass, 40000);
