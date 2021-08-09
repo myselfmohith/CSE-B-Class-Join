cf5e70c87ba7bbb81c64df7b4d043d921845fc5b
myselfmohith <bezawadamohith14@gmail.com>
2021-08-09 13:55:28 +0530

    Added Service Worker File

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
 // Service Worker Version 0.1
+const CACHE_NAME = "cse-b-cache-v0.1";
+const assets = ["./offline.html"]
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                cache.addAll(assets);
+            })
+    )
+})
+
+self.addEventListener('activate', event => {
+    console.log('Activating Service Worker');
+})
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(res => res || fetch(event.request).catch(() => caches.match("./offline.html")))
+    )
+})
