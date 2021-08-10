9e2da95b470ee5019254c6a002c65d4264062904
myselfmohith <bezawadamohith14@gmail.com>
2021-08-10 12:40:19 +0530

    10/08 - Added Notification Support

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// Check for Notification Permisiions
+let notificationPermission = Notification.permission === 'granted';
+Notification.permission === "default" && Notification.requestPermission((res) => notificationPermission = res === "granted");
+
+let userName = localStorage.getItem('user-name') || null;
+
+function notifyClass(className) {
+    if (!notificationPermission) return;
+    const currentTime = new Date();
+    const prevNotification = JSON.parse(localStorage.getItem('notification-track') || null);
+    if (prevNotification) {
+        if (currentTime.getTime() < new Date(Number(prevNotification.expiry)) && className === prevNotification.className) return;
+    };
+    const options = {
+        body: `Hey ${userName}.\nYou have "${className}" in few Minutes`,
+        icon: './icons/icon.png',
+    }
+    serviceWorker.showNotification('CSE-B Class Alert', options);
+    localStorage.setItem('notification-track', JSON.stringify({
+        expiry: currentTime.getTime() + 2700000,
+        className
+    }))
+}
+
+
+
 function customFormatToTime(timeString) {
     const [hr, min] = timeString.split(":").map(i => Number(i));
     const time = new Date();
@@ -19,8 +45,19 @@ function runEachSecond() {
     }
     if (cls) {
         document.getElementById('current-class').innerHTML = `<p>Current Class</p><h1><a href="${jsonData.links[cls]}" target="blank" >${cls}</a></h1>`;
+        notifyClass(String(cls));
     }
 };
 
 runEachSecond();
 setInterval(runEachSecond, 1000);
+
+
+// Get user deatils
+if (!userName) {
+    userName = prompt('Enter Your username');
+    if (userName === "") userName = null;
+    else {
+        localStorage.setItem('user-name', userName);
+    }
+}
--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Service Worker Version 0.1
-const CACHE_NAME = "10/08 - Moved Course Details Up";
+const CACHE_NAME = "10/08 - Added Notification Support";
 const assets = [
     "./",
     "./icons/icon.png",
