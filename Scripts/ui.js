faf3da17b31b9800cd33f8f9ea5b102f7bc87124
Mohith Kumar Bezawada <76643238+myselfmohith@users.noreply.github.com>
2021-08-21 07:57:56 +0530

    21/08 Saturday = Friday

--- Scripts/ui.js
+++ Scripts/ui.js
@@ -29,7 +29,7 @@ for (var i = 0; i < classStart.length; i++) appendTxt += `<th>${classStart[i]} -
 appendTxt += "</tr>";
 
 const timetable = appData['timetable'];
-timetable[6] = timetable[56] || null;
+timetable[6] = timetable[5] || null;
 
 for (var i = 0; i < timetable.length; i++) {
     if (!timetable[i]) continue;
